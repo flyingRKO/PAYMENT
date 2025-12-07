7a0e0ecf99b2d9d736212223abdd7865fb14142b
이재훈 <wogns970@naver.com>
2025-12-07 23:07:44 +0900

    db lock으로 데이터 무결성 보장하기

--- src/main/java/com/example/PAYMENT/wallet/WalletRepository.java
+++ src/main/java/com/example/PAYMENT/wallet/WalletRepository.java
@@ -1,10 +1,14 @@
 package com.example.PAYMENT.wallet;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Wallet> findTopByUserId(Long userId);
     Optional<Wallet> findAllByUserId(Long userId);
 }
